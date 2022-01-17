1a056931313ef04ca5a58f2dd858078bfbadbfbe
Consgomez <a01026717@itesm.mx>
2022-01-16 20:05:10 -0600

    classes

--- Source/reverb_HeaderGUI.h
+++ Source/reverb_HeaderGUI.h
@@ -1,11 +1,21 @@
-/*
-  ==============================================================================
-
-    reverb_HeaderGUI.h
-    Created: 16 Jan 2022 7:43:23pm
-    Author:  Constanza Gómez Sánchez
-
-  ==============================================================================
-*/
-
 #pragma once
+#include <JuceHeader.h>
+#include "PluginProcessor.h"
+
+class reverb_HeaderGUI : public juce::Component
+{
+public:
+    reverb_HeaderGUI(Reverb_MasterClassAudioProcessor&);
+    
+    ~reverb_HeaderGUI() override;
+    
+    void paint(juce::Graphics&) override;
+    
+    void resized() override;
+    
+private:
+    
+    Reverb_MasterClassAudioProcessor& audioProcessor;
+    
+    JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR(reverb_HeaderGUI)
+};
--- Source/reverb_MainGUI.h
+++ Source/reverb_MainGUI.h
@@ -1,11 +1,21 @@
-/*
-  ==============================================================================
-
-    reverb_MainGUI.h
-    Created: 16 Jan 2022 7:43:32pm
-    Author:  Constanza Gómez Sánchez
-
-  ==============================================================================
-*/
-
 #pragma once
+#include <JuceHeader.h>
+#include "PluginProcessor.h"
+
+class reverb_MainGUI : public juce::Component
+{
+public:
+    reverb_MainGUI(Reverb_MasterClassAudioProcessor&);
+    
+    ~reverb_MainGUI() override;
+    
+    void paint(juce::Graphics&) override;
+    
+    void resized() override;
+    
+private:
+    
+    Reverb_MasterClassAudioProcessor& audioProcessor;
+    
+    JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR(reverb_MainGUI)
+};
